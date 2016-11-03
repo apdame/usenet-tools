799d3480b62b7b5f995b200a3280c5a6ff969522
Avery Dame <adame@umd.edu>
2016-11-03 10:37:55 -0400

    Update addresscounter.py

--- addresscounter.py
+++ addresscounter.py
@@ -9,27 +9,22 @@ from sys import argv
 import csv
 import mailbox
 from collections import Counter
-import os
 
 address = []
-script, box, csvfile = argv                  #box = Usenet archive you're working with, csvfile = name of CSV file results will output to.
+script, box, csvfile = argv
+#box = Usenet archive you're working with
+#csvfile = name of CSV file results will output to.
 
-mbox = mailbox.mbox(box)
-
-with open('addresslist.txt','w') as w:       #Collects addresses into a list and stores them in a .txt file
-    for message in mbox:
-        address.append(message['from'])
-    for item in address:
-        w.writelines(str(item).strip() + "\n")
-
-with open('addresslist.txt','r') as addlist, open(csvfile, 'wb+') as csv_file:           #Writes addresses to CSV file
-    ngwriter = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL,
+with open(box,'r') as addlist, open(csvfile, 'wb') as csv_file:           
+    writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL,
                           quotechar=' ', skipinitialspace=False, escapechar='\\')
-    text = addlist.readlines()
-    duplicates = Counter(text)
+    for message in mbox:
+        address.append(str(message['from']).replace("#1/1", ""))
+    for item in address:
+        item = ''.join(item.split())
+    #Identifies unique addresses
+    duplicates = Counter(address)
+    writer.writerow(["Address"] + ["Count"])
     for key, value in duplicates.iteritems():
-            ngwriter.writerow([str(key).strip()] + [str(value).strip()])
-
-os.remove('addresslist.txt')                                      #Deletes txt file where addresses are stored.
-
-
+        #Writes addresses to CSV file
+        writer.writerow([str(key)] + [str(value)])
