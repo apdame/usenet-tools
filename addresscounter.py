e499e93d5501594c20def374673198ed3adfdee6
Avery Dame <adame@umd.edu>
2016-11-03 11:36:27 -0400

    Update addresscounter.py

--- addresscounter.py
+++ addresscounter.py
@@ -5,23 +5,28 @@ file, then uses text file to build total count
 of unique emails.
 '''
 
-from sys import argv
 import csv
 import mailbox
+import email.message
 from collections import Counter
 
 address = []
-script, box, csvfile = argv
-#box = Usenet archive you're working with
-#csvfile = name of CSV file results will output to.
 
-with open(box,'r') as addlist, open(csvfile, 'wb') as csv_file:           
+mbox = mailbox.mbox(raw_input("What is the mailbox you want to work with? "))
+print "Reading File..."
+
+for message in mbox:
+    msg = str(message["From"])
+    address.append(msg)
+for item in address:
+    item = ''.join(item.split())
+
+csvf = raw_input("What is the name of the csv file it should output to? ")
+print "Writing To File..."
+
+with open(csvf, 'wb') as csv_file:           
     writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL,
                           quotechar=' ', skipinitialspace=False, escapechar='\\')
-    for message in mbox:
-        address.append(str(message['from']).replace("#1/1", ""))
-    for item in address:
-        item = ''.join(item.split())
     #Identifies unique addresses
     duplicates = Counter(address)
     writer.writerow(["Address"] + ["Count"])
