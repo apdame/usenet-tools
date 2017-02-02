42bb7be525a0d9b19edb0a9d5acda6f8f957782d
Avery Dame <adame@umd.edu>
2017-02-02 08:27:10 -0500

    Fixing minor errors

--- crosspost.py
+++ crosspost.py
@@ -6,30 +6,30 @@ then inputs to .csv for analysis.
 
 import mailbox
 import csv
+from string import whitespace
 
 crosspost = {}
 
-mbox = mailbox.mbox(raw_input("What is the mailbox you want to work with? "))
+mbox = mailbox.mbox(raw_input("What is the mailbox you want to work with? (Please include file extension.) "))
 csvf = raw_input("What is the name of the csv file it should output to? ")
 
 print "Reading File..."
 
 for message in mbox:
-    msg = str(message['Message-ID']).replace("#1/1", "").strip()
-    msg += ","
+    msg = str(message['Message-ID']).replace("#1/1", "").translate(None, whitespace)
     #collects Message-ID and cleans it up a bit
-    ng = str(message['Newsgroups']).split(',')
+    ng = str(message['Newsgroups']).translate(None, whitespace).split(',')
     #Alphabetizes list for uniformity. You can omit the
     #next two lines if you want to get the lists "as is."
     ngs = sorted(ng)
-    ng = ','.join(ngs)
+    nglist = '; '.join(ngs)
     #Sets ID as key, with Alpha Newsgroups as value
-    crosspost[msg] = ng
+    crosspost[msg] = nglist
 
 print "Writing To File..."
 
-with open(csvf, 'wb') as csv_file:
-    writer = csv.writer(csv_file, delimiter=' ', quotechar=' ', quoting=csv.QUOTE_MINIMAL)
+with open("%s.csv" % csvf, 'wb') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL, quotechar=' ', skipinitialspace=False, escapechar='\\')
     writer.writerow(["Message ID"] + ["Newsgroups"])
     for key, value in crosspost.iteritems():
         #writes row with Message ID and
