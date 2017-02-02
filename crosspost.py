975fb90c63df063f11d792eb92034e9d6b5f32c7
Avery Dame <adame@umd.edu>
2017-02-02 08:39:41 -0500

    Update crosspost.py

--- crosspost.py
+++ crosspost.py
@@ -10,30 +10,35 @@ from string import whitespace
 
 crosspost = {}
 
-mbox = mailbox.mbox(raw_input("What is the mailbox you want to work with? (Please include file extension.) "))
-csvf = raw_input("What is the name of the csv file it should output to? ")
-
-print "Reading File..."
-
-for message in mbox:
+def ngcount(message):
     msg = str(message['Message-ID']).replace("#1/1", "").translate(None, whitespace)
     #collects Message-ID and cleans it up a bit
     ng = str(message['Newsgroups']).translate(None, whitespace).split(',')
     #Alphabetizes list for uniformity. You can omit the
     #next two lines if you want to get the lists "as is."
     ngs = sorted(ng)
-    nglist = '; '.join(ngs)
+    nglist = ';'.join(ngs)
     #Sets ID as key, with Alpha Newsgroups as value
     crosspost[msg] = nglist
 
-print "Writing To File..."
+def main():  
+    mbox = mailbox.mbox(raw_input("What is the mailbox you want to work with? (Please include file extension.) "))
+    csvf = raw_input("What is the name of the csv file it should output to? ")
 
-with open("%s.csv" % csvf, 'wb') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL, quotechar=' ', skipinitialspace=False, escapechar='\\')
-    writer.writerow(["Message ID"] + ["Newsgroups"])
-    for key, value in crosspost.iteritems():
-        #writes row with Message ID and
-        #list of Newsgroups, 1 newsgroup per cell
-        writer.writerow([key] + [value])
+    print "Reading file..."
+    
+    for message in mbox:
+        ngcount(message)
+    
+    with open("%s.csv" % csvf, 'wb') as csv_file:
+        print "Writing To File..."
+        writer = csv.writer(csv_file, delimiter=',', quoting=csv.QUOTE_MINIMAL, quotechar=' ', skipinitialspace=False, escapechar='\\')
+        writer.writerow(["Message ID"] + ["Newsgroups"])
+        for key, value in crosspost.iteritems():
+            #writes row with Message ID and
+            #list of Newsgroups, 1 newsgroup per cell
+            writer.writerow([key] + [value])
+        print "Writing Complete!"
 
-print "Writing Complete!"
+if __name__=="__main__":
+    main()
